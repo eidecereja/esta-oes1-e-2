304867bf8c3460a2e76ab3eb5b5c8f30aa5d4958
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-13 20:07:56 -0300

    main

--- main.js
+++ main.js
@@ -16,6 +16,6 @@ const button=
 document.querySelectorAll(“button”);
 let contador =0;
 wile(contador<buttons.length){
-buttons[contador].textContent=contador;
+buttons[contador].textContent = contador;
 contador = contador + 1;}
 
