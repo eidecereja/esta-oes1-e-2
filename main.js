f053b313d05ff6efc66d3871aded48960b23c803
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-13 19:51:01 -0300

    estação3

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ Const aplausos=document.querySelector
 (“#som_tecla_aplausos”);
 Function tocaSom(aplausos){alert(aplausos);}
 tocaSom(“clap,clap,clap”);
+
+let comando= “tocar”
+if(comando== “tocar”){
+alert( “Áudio pronto para tocar”);}
+else{
+alert(“O áudio não está pronto”);}
