599aec88c0363048cb41b5556205fadf73d32231
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-13 19:54:29 -0300

    alterando estaçao 3

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ Function tocaSom(aplausos){alert(aplausos);}
 tocaSom(“clap,clap,clap”);
 
 let comando= “tocar”
-if(comando== “tocar”){
-alert( “Áudio pronto para tocar”);}
-else{
-alert(“O áudio não está pronto”);}
+if(comando== “tocar”)
+{
+alert(“Áudio pronto para tocar”);}
+else
+{
+alert(“O áudio não está pronto”);
+}
