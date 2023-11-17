4aed2a9de9a77bc235e57cecf3c48498bbba2a73
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-17 17:15:58 -0300

    main

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-const aplausos=document.querySelector
-(“#som_tecla_aplausos”);
+const aplausos = document.querySelector("#som_tecla_aplausos");
 function tocaSom(aplausos){
-alert(aplausos);}
-tocaSom(“clap,clap,clap”);
-
-let comando= “tocar”
-if(comando== “tocar”)
-{
-alert(“Áudio pronto para tocar”);}
-else
-{
-alert(“O áudio não está pronto”);
+    alert(aplausos);
 }
-
-const button= document.querySelectorAll(“button”);
-let contador =0;
-wile(contador<buttons.length){
-buttons[contador].textContent = contador;
-contador = contador + 1;}
 
