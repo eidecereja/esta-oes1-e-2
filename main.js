537f377f43061d4135b3776ba3c8999b7c11a7ae
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-22 13:27:36 -0300

    alterando main

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const aplausos = document.querySelector("#som_tecla_aplausos");
 function tocaSom(aplausos){
     alert(aplausos);
 }
-
+function digaSeuNome(Eide){
+    alert(Eide);
+}
+digaSeuNome("Meu nome é EIDE");
